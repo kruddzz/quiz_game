e12624b794098dbd2cb1aff36c5c4ab9e307f84f
Kyle Rudderforth <kruddzz@gmail.com>
2020-06-17 04:19:24 -0500

    questions.js upload

---
+++ assets/questions.js
@@ -0,0 +1,54 @@
+var questions = [
+    {
+      title: "Which is an example of a non-sematic element:",
+      choices: ["<form>", "<div>", "<article>", "<table>"],
+      answer: "<div>"
+    },
+    {
+      title: "In the Box Model what does padding do?",
+      choices: ["Clears an area around the content.", "Clears an area outside the border.", "The area of the box, where text and images appear", " A border that goes around the padding and content"],
+      answer: "Clears an area around the content."
+    },
+    {
+      title: "Which is a Data Type of JavaScript?",
+      choices: ["number", "string", "object", "All answers are JavaScript Data Types"],
+      answer:  "All answers are JavaScript Data Types"
+    },
+    {
+      title: "What JavaScript data type can only be true or false?",
+      choices: ["Boolean", "String", "Object", "Number"],
+      answer: "Boolean"
+    },
+    {
+      title: "What is an array?",
+      choices: ["It's used for storing and manipulating text.", "A special variable, which can hold more than one value at a time.", "A block of code designed to perform a particular task.", "None of these answers are correct."],
+      answer: "A special variable, which can hold more than one value at a time."
+    },
+    {
+      title: "The greater than/or equal operator is:",
+      choices: [">>", "<=", "+=", ">="],
+      answer: ">="
+    },
+    {
+      title: "Inside which HTML element do we put the JavaScript?",
+      choices: ["<javascript>", "<script>", "<js>", "<link>"],
+      answer: "<script>"
+    }, 
+    {
+      title: "What html tag is NOT included in the HEAD tag?",
+        choices: ["link", "meta", "title", "header"],
+        answer: "header"
+    },
+    {
+      title: "An array is enclosed in:",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "square brackets"
+    },
+    {
+      title: "Adds new elements to the beginning of an array, and returns the new length",
+      choices: ["unshift()", "Operators", ".innerHTML", "Do While Loop"],
+      answer: "unshift()"
+    },
+    
+  ];
+  
