dd256b8d018d5b3b845db1e288ba7d374b4fa40b
Kyle Rudderforth <kruddzz@gmail.com>
2020-06-16 20:47:00 -0500

    start quiz setup in script.js

--- assets/script.js
+++ assets/script.js
@@ -18,18 +18,53 @@ var quizS = document.getElementById("#Score");
 var int = document.getElementById("#initials");
 var submitB = document.getElementById("#submitBtn");
 
+var numCorrectAnswers = 0;
+var numTotalQuestions = 0;
+var intQuestion = 0;  // first question starts at 0
+var blnCorrect = false;
+var endScore = 0;
+var finQuestion = false;
 
+takeQ.addEventListener("click", function (event) {
+  var element = event.target;
+  if (element.matches("button")) {
+    //which button did they click
+    // console.log("button clicked: " + element.id);
+    
+
+    quizR.textContent = element.getAttribute("data-answered");
+    quizR.style.color = "red";
+    // console.log(quizR);
+    if (element.getAttribute("data-answered") === "Correct") {
+      blnCorrect = true;
+      quizR.style.color = "green";
+      numCorrectAnswers++;
+    } else {
+      // incorrect answer, penalize them 5 second
+      secondsLeft -= 5;
+      checkTimeRemaining();
+
+    }
+    
+    // load the next question
+    intQuestion++;
+    loadQuestion();
+  }
+
+});
 
 
 // Event Listeners
-startB.addEventListener("click", funtion(event)) {
-    
+startB.addEventListener("click", funtion(event) {
+
   // user clicks the Start Quiz button
-  // hide the startQuiz section and show the takeQuiz section
+  // hide the Start Page section and show the TakeQuiz section
 
   if (event === null) {
     return;
   }
 
   takeQuiz();
-}
+});
+
+
