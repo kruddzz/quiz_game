1e1c06e62952ec30f7df087a9f8e54273299c877
Kyle Rudderforth <kruddzz@gmail.com>
2020-06-16 19:03:43 -0500

    var grabbed from html setup in script.js

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+// Variables
+var clock = document.getElementById("timer");
+var secondsLeft = 75;
+var clockInterval;
+
+var startQ = document.getElementById("#startQuiz");
+var takeQ = document.getElementById("#takeQuiz");
+var qn = document.getElementById("#question");
+var startB = document.getElementById("#startBtn");
+var answ1 = document.getElementById("#btnA1");
+var answ2 = document.getElementById("#btnA2");
+var answ3 = document.getElementById("#btnA3");
+var answ4 = document.getElementById("#btnA4");
+var quizR = document.getElementById("#result");
+var fScore = document.getElementById("#finalScore");
+var quizD = document.getElementById("#quizDone");
+var quizS = document.getElementById("#Score");
+var int = document.getElementById("#initials");
+var submitB = document.getElementById("#submitBtn");
+
+
+
+
+
+// functions
+
+// event listeners
